4b1c4c828ab1ad5960e821b0f49c6f75b548eb5d
gbati <gheorghe835@gmail.com>
2024-03-21 21:23:34 +0200

    added lesson2:task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+int num = new Random().Next(100,1000);
+Console.WriteLine($"  Number :: {num}");
+int numHundreds = num / 100;
+int numUnits = num % 10;
+int numTens = num / 10 % 10;
+Console.WriteLine(" -------- ");
+Console.WriteLine($" Hundreds :: {numHundreds}");
+Console.WriteLine($" Tens :: {numTens} ");
+Console.WriteLine($" Units :: {numUnits}");
+Console.WriteLine(" -------- ");
+Console.WriteLine($"{numHundreds}{numUnits}");
+Console.WriteLine($"{numHundreds}{numTens}{numUnits}");
+
+string num_str = Convert.ToString(num);
+Console.WriteLine(Convert.ToString(num_str[0])+
+                  Convert.ToString(num_str[2]));
+
--- task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
+++ task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84adfa6fe250d5baf853684c9c0a0273936f7d68")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
