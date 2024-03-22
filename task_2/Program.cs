2756f349cfe3295a712468259217ad6b833c4307
gbati <gheorghe835@gmail.com>
2024-03-22 19:57:12 +0200

    added new result lesson2:task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -6,8 +6,14 @@ int num = new Random().Next(100,1000);
 Console.WriteLine($"  Number :: {num}");
 int numTens = num / 10 % 10;
 int numUnits = num % 10;
-int result;
-
-
+Console.WriteLine(" -------- ");
 Console.WriteLine($" Result == {Math.Pow(numTens,numUnits)}");
-
+Console.WriteLine(" -------- ");
+int i = 1;
+int c = numTens;
+while (i < numUnits)
+{
+    c *= numTens;
+    Console.WriteLine(c);
+    i++;
+}
--- task_2/obj/Debug/net8.0/task_2.AssemblyInfo.cs
+++ task_2/obj/Debug/net8.0/task_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b1c4c828ab1ad5960e821b0f49c6f75b548eb5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9528ad550a8d412ffe5dd4da365adbfde8e42299")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
