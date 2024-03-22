9528ad550a8d412ffe5dd4da365adbfde8e42299
gbati <gheorghe835@gmail.com>
2024-03-22 19:41:38 +0200

    added lesson2:task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+int num = new Random().Next(100,1000);
+Console.WriteLine($"  Number :: {num}");
+int numTens = num / 10 % 10;
+int numUnits = num % 10;
+int result;
+
+
+Console.WriteLine($" Result == {Math.Pow(numTens,numUnits)}");
+
