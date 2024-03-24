46ea69dd65449e77e9a1d53c99ec13d56ff83b19
gbati <gheorghe835@gmail.com>
2024-03-24 08:54:18 +0200

    added lesson2:task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string Test(int first,int second)
+{
+    if(second == 0){return " Devision by 0!!!";}
+    int result = first % second;
+    if(result == 0){return " YES ";}
+    else{ return ($" NO, {result}"); }
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Test(a,b));
