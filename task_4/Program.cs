6d57cb1867109e4a28349132321b0b88f9f3c6c0
gbati <gheorghe835@gmail.com>
2024-03-24 09:21:24 +0200

    added lesson2:task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int num = new Random().Next(100,10000);
+Console.WriteLine($"  Number :: {num}");
+int res;
+if(num > 100 && num < 1000)
+{
+    res = num % 10;
+    Console.WriteLine($"  Number :: {res}");
+}
+if(num > 1000 && num < 10000)
+{
+    res = num / 10 % 10;
+    Console.WriteLine($"  Number :: {res}");
+}
+else
+{
+     Console.WriteLine("The third number doesn't exist");
+}
